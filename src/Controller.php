9b11a179f8fa9d9a5d6ec8c6fc898733ef4639d9
ortemx <printerq@yandex.ru>
2023-10-07 21:59:28 +0300

    update for version 1.2.0

--- src/Controller.php
+++ src/Controller.php
@@ -7,6 +7,7 @@ use function ortemx\GuessNumber\View\showWelcomeMessage;
 use function ortemx\GuessNumber\Model\guessNumber;
 use function cli\line;
 use function cli\input;
+
 define("MAX_NUMBER", 100);
 define("NUMBER_OF_ATTEMPTS", 10);
 
@@ -14,18 +15,15 @@ function loop()
 {
     $attempt_number = 0;
     $guested_number = guessNumber();
-    while ($attempt_number < NUMBER_OF_ATTEMPTS)
-    {
+    while ($attempt_number < NUMBER_OF_ATTEMPTS) {
         $input_number = input();
-        if ($input_number == $guested_number)
-        {
+        if ($input_number == $guested_number) {
             line("Поздравляем! Вы угадали число");
             return;
-        } else if ($input_number == 0){
+        } elseif ($input_number == 0) {
             line("Вы вышли из игры");
             return;
-        } else if ($input_number > $guested_number)
-        {
+        } elseif ($input_number > $guested_number) {
             line("Загаданное число меньше");
         } else {
             line("Загаданное число больше");

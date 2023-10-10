7745af8355d457fab2cdb3697c0ab4378887d5b8
ortemx <printerq@yandex.ru>
2023-10-10 20:27:52 +0300

    Обнавленна автозагрузка

--- bin/guess-number.php
+++ bin/guess-number.php
@@ -1,8 +1,10 @@
 #!/usr/bin/env php
 
 <?php
+$GithubPath = __DIR__.'/../vendor/autoload.php';
+$PackagistPath = __DIR__.'/../../../autoload.php';
 
-require __DIR__ . '/../vendor/autoload.php';
+require file_exists($GithubPath) ? $GithubPath : $PackagistPath;
 
 use function ortemx\GuessNumber\Controller\startGame;
 
